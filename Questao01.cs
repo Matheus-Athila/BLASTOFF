e72f4b4bb9c5931b191b9e46720050b7c02d716b
Matheus-Athila <mathila64@gmail.com>
2022-02-15 22:52:30 -0300

    questao01 - Média idades

---
+++ Questao01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blastoff
+{
+    internal class Questao01
+    {
+        private static void Main(string[] args)
+        {
+            int soma, media;
+
+            soma = 1 + 1 + 1 + 1 + 1;
+            media = soma / 5;
+            Console.WriteLine("soma = " + soma);
+
+            Console.WriteLine("Media = " + media);
+
+            Console.ReadKey();
+
+        }
+    }
+}
