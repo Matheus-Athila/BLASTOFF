6c3afa8c27111e9f3e777b1d4a42eaceb355ce30
Matheus-Athila <mathila64@gmail.com>
2022-02-15 23:31:42 -0300

    Questao02 - Media combustivel

---
+++ Questao02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blastoff
+{
+    internal class Questao02
+    {
+        private static void Main(string[] args)
+        {
+            int Distanca, CombustivelGasto;
+
+            Distanca = 5;
+            CombustivelGasto = 5;
+
+            int MediaDeConbustivelGastoPorDistancia = Distanca / CombustivelGasto;
+
+            Console.WriteLine("MediaDeCombustivelGastoPorDistancia = " + MediaDeConbustivelGastoPorDistancia);
+            Console.ReadKey();
+        }
+    }
+}
